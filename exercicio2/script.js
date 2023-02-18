f1fecab9294a94e39e7cebfd0e61d743d8ad4994
j0rdanaaa <j0rdanaaa@gmail.com>
2023-02-18 12:33:45 -0300

    exercícios

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,27 @@
+//a
+
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+
+const retirarEspacos = (frase) =>{
+ const novaFrase = frase.trim()
+
+ console.log(novaFrase)
+return novaFrase
+}
+
+const novaSting = retirarEspacos(minhaString)
+
+// item A com função:
+
+//b
+
+console.log(minhaString.length)
+console.log(novaSting.length)
+
+//c
+
+const substituicao = novaSting.replaceAll('________', 'sticioso')
+console.log(substituicao)
+
+
